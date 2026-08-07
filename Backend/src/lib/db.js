0e2f9b58c0c54ede0531ec3c4ef8848217aec6fb
Rohit Singh <rohitsingh7667142299@gmail.com>
2026-08-08 01:44:04 +0530

    inngest config

--- Backend/src/lib/db.js
+++ Backend/src/lib/db.js
@@ -4,6 +4,9 @@ import {ENV} from './env.js';
 
 export const connectDB=async () => {
     try{
+        if(!ENV.DB_URL){
+            throw "ENV.DB_URL is not defined in Env files";
+        }
         const conn=await mongoose.connect(ENV.DB_URL);
         console.log("✔️ Connected to MongoDB:",conn.connection.host)
 
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,11 +1,24 @@
 import express from "express";
 import path from "path";
 import fs from "fs";
+import cors from "cors";
+import {server} from "inngest/express";
 import {connectDB} from './lib/db.js';
 import { ENV } from "./lib/env.js";
+import { inngest } from "./lib/inngest.js";
+
+
 
 
 const app = express();
+
+//middleware 
+app.use(express.json());
+
+//credentials:true meaning ?? => server allows a browser to include cookies on req   
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+
+app.use("/api/inngest",serve({client:inngest,functions})); 
 
 const root = path.resolve();
 
